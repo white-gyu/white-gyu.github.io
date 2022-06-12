deeef789f99d117bf247994054904cde91298c8b
^__^ BaekGyu <hana222966@naver.com>
2022-06-13 00:16:12 +0900

    feat: resume open graph thumbnail

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import { Head } from '../components/head'
 import { rhythm } from '../utils/typography'
 import * as Lang from '../constants'
 
@@ -14,18 +15,25 @@ export default ({ data }) => {
     .map(({ node }) => node)[0]
 
   return (
-    <div className="about"
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(28),
-        padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
-          3 / 4
-        )}`,
-      }}
-    >
+      <>
+        <Head
+            title={"About white-gyu"}
+            description={"white-gyu Resume"}
+            thumbnail={"https://user-images.githubusercontent.com/67765871/173239925-528506e0-9d6a-456b-8df3-5551fa43a503.png"}
+        />
+        <div className="about"
+            style={{
+                marginLeft: `auto`,
+                marginRight: `auto`,
+                maxWidth: rhythm(28),
+                padding: `${rhythm(0.5)} ${rhythm(3 / 4)} ${rhythm(1.5)} ${rhythm(
+                    3 / 4
+                )}`,
+            }}
+        >
       <div dangerouslySetInnerHTML={{ __html: resume.html }} />
     </div>
+    </>
   )
 }
 
